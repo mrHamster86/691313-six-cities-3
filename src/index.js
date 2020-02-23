8038d8a7857b58e2ef012cbc164bc18e8408c715
Evgeniy <svolochina1@yandex.ru>
2020-02-23 22:43:10 +0300

    Личный проект: начинаем программировать

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './components/app/App.jsx';
 
+const root = document.getElementById(`root`);
+
+const placesCount = 312;
+
+ReactDOM.render(<App placesCount={placesCount}/>, root);
